9c1b554debd156c151090305209c7296de210cb3
yotsarunkia <yotsarun.kia@student.mahidol.edu>
2026-02-07 18:03:26 +0000

    makefile

--- main.cpp
+++ main.cpp
@@ -6,33 +6,33 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
+  //setup
   int *a, N;
-  /* Make sure you convert 'a' properly*/
+  
+//convert to array and size
   N = argc - 2;
   a = new int[N];
+
   for (int i = 0; i < N; i++)
   {
     a[i] = atoi(argv[i + 2]);
   }
 
   display(a, N);
-
+//determining sorting type
   if (argc > 1)
   {
     if (strcmp(argv[1], "bubble") == 0)
     {
       bubbleSort(a, N);
-      display(a, N);
     }
     else if (strcmp(argv[1], "insert") == 0 )
     {
       insertion(a, N);
-      display(a, N);
     }
     else if (strcmp(argv[1], "selection") == 0)
     {
       selectionSort(a, N);
-      display(a, N);
     }
     cout<<"Result"<<endl;
     display(a, N);
