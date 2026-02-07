01a4261a318402a1f2ffc1db50e168a29179e5a5
yotsarunkia <yotsarun.kia@student.mahidol.edu>
2026-02-07 15:17:37 +0000

    sorting.h

--- sorting.h
+++ sorting.h
@@ -1,60 +1,84 @@
 #ifndefine SORTING_H
 #define SORTING_H
-
+using namespace std;
 #include <iomanip>
 /* function prototype*/
 
+void display(int a[], int n)
+{
 
-void display(int a[],int n){
+  int i;
 
-    int i;
+  for (i = 0; i < n; i++)
+    cout << setw(3) << a[i];
 
-    for(i=0;i<n;i++)
-        std::cout<<setw(3)<<a[i];
-
-	std::cout<<std::endl;
-  
+  cout << endl;
 }
 
-void selectionSort(int data[], int length) 
-{ 
-	int i, j, m, mi; 
-	
-     display(data,length);
-     
-	
-} 
+// slect
 
-
-
-void insertion(int a[],int n){
-
-
+void selectionSort(int a[], int n)
+{
+  int i, j, m, mi;
+  for (i = 0; i < n ; i++)
+  {
+    mi = i ;
+    for (j = i+1; j < n ; j++)
+    {
+      if (a[j]<a[mi])
+      {
+        mi = j;
+      }
+    }
+    swap(a[i],a[mi]);
+  }
+  display(a, n);
 }
 
-void swap(int &a,int &b){
- int temp;
-  temp=a;
-  a=b;
-  b=temp;
-  
+// insertion
+
+void insertion(int a[], int n)
+{
+  int i;
+  for (i = 1; i < n; i++)
+  {
+    int obj = a[i];
+    int j = i - 1;
+    while (a[j] > obj)
+    {
+      a[j + 1] = a[j];
+      j--;
+    }
+    a[j + 1] = obj;
+  }
 }
 
-void bubbleSort(int a[],int n){
+// swap
 
-int i,j;
-int sorted;
-// how may pair to compare?
-for(j=1;j<n;j++){
+void swap(int &a, int &b)
+{
+  int temp;
+  temp = a;
+  a = b;
+  b = temp;
+}
 
+void bubbleSort(int a[], int n)
+{
 
-	
-  display(a,n);
- }
-  
+  int i, j;
+  int sorted;
+  // how may pair to compare?   
+  for (j = 0 ; j < n; j++)
+  {
+    for (i=0;i<n-j;i++)
+    {
+      if (a[i]>a[i+1])
+      {
+        swap(a[i],a[i+1]);
+      }
+    }
+    display(a, n);
+  }
 }
 #endif
-
-
-
-
