3f42e40a803800337b6cfdbf985775a3bd0ca8c0
yotsarunkia <yotsarun.kia@student.mahidol.edu>
2026-02-07 19:54:21 +0000

    main.cpp

--- sorting.h
+++ sorting.h
@@ -21,22 +21,21 @@ void display(int a[], int n)
 void selectionSort(int a[], int n)
 {
   int i, j, m, mi;
-  for (i = 0; i < n ; i++)
+  for (i = 0; i < n; i++)
   {
-    mi = i ;
-    for (j = i+1; j < n ; j++)
+    mi = i;
+    for (j = i + 1; j < n; j++)
     {
-      if (a[j]>a[mi])
+      if (a[j] > a[mi])
       {
         mi = j;
       }
     }
-    
-    swap(a[i],a[mi]);
+
+    swap(a[i], a[mi]);
     display(a, n);
-    cout<<endl;
+    cout << endl;
   }
-  
 }
 
 // insertion
@@ -49,17 +48,15 @@ void insertion(int a[], int n)
     int obj = a[i];
     int j = i - 1;
 
-    while ( j>= 0 && a[j] < obj)
+    while (j >= 0 && a[j] < obj)
     {
       a[j + 1] = a[j];
       j--;
       display(a, n);
-      
     }
     a[j + 1] = obj;
     display(a, n);
-    cout<<endl;
-
+    cout << endl;
   }
 }
 
@@ -75,22 +72,37 @@ void swap(int &a, int &b)
 
 void bubbleSort(int a[], int n)
 {
-
   int i, j;
-  int sorted;
+  int sorted=0;
   // how may pair to compare?   
   for (j = 0 ; j < n-1; j++)
   {
+
     for (i=0;i<n-j-1;i++)
     {
       if (a[i]<a[i+1])
       {
         swap(a[i],a[i+1]);
         display(a, n);
+        for (int c = 0; c < n - 1; c++)
+          {
+            if (a[c] < a[c + 1])
+            {
+              sorted = 0;
+              break;
+            }
+            sorted = 1;
+          }
+        
+        
       }
-      
     }
+    if (sorted == 1) break;
     cout<<endl;
+    display(a, n);
+    
+    
+
   }
 }
 #endif
